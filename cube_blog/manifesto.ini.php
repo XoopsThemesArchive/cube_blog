529cd733136925f4683342d5529e6e2656130f7b
mambax7 <mambax7@gmail.com>
2015-05-04 05:44:04 -0400

    Original files

---
+++ cube_blog/manifesto.ini.php
@@ -0,0 +1,21 @@
+<?php
+/**
+
+[Manifesto]
+Name="Cube Blog"
+Depends=Legacy_RenderSystem,legacy
+Url="http://xoopscube.org"
+Version="1.0.0"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=Nuno Luciano
+ScreenShot="screenshot.png"
+Description="Cube Blog theme for XOOPS Cube 2.1 Legacy"
+W3C=NG
+
+Licence="http://creativecommons.org/licenses/by-nc-nd/2.0/"
+
+*/
+?>
---
+++ cube_blog/txc_themeinfo.php
@@ -0,0 +1,7 @@
+<?php
+	$txc_themeinfo["name"] = "cube_blog";
+	$txc_themeinfo["author"] = "Nuno Luciano";
+	$txc_themeinfo["author_url"] = "http://www.xoopserver.com";
+	$txc_themeinfo["detail_url"] = "http://creativecommons.org/licenses/by-nc-nd/2.0/";
+	$txc_themeinfo["download_url"] = "http://www.xoopserver.com";
+?>
